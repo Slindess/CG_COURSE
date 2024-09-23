38e48ddec3034fb36d56b49f08566c84968173f2
Mikhail Slinyakov <mikhail.slinyakov@qoollo.com>
2024-09-23 23:40:59 +0300

    Camera

--- source/Camera/Camera.h
+++ source/Camera/Camera.h
@@ -7,7 +7,21 @@
 
 
 class Camera {
+public:
+    /* ТУТ ЦЕНТР ЭКРАНА ПРОЕЦИРОВАНИЯ */
+    double x_screen;
+    double y_screen;
+    double z_screen;
 
+    /* ТОЧКА ВЗГЛЯДА КАМЕРЫ */
+    double x_view;
+    double y_view;
+    double z_view;
+
+    /* АВИАЦИЯ */
+    double pitch; // Тангаж
+    double yaw; // Рыскание
+    double roll; // Крен
 };
 
 
